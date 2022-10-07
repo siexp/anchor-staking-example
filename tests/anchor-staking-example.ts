b131ebad4e89b51ed54e6fb085e72c72bc57f923
Mark Tsyrulnyk <elioni17@gmail.com>
2022-10-07 16:50:27 +0300

    tests

--- tests/anchor-staking-example.ts
+++ tests/anchor-staking-example.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { AnchorStakingExample } from "../target/types/anchor_staking_example";
+import { expect } from 'chai'
 
 describe("anchor-staking-example", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +9,26 @@ describe("anchor-staking-example", () => {
 
   const program = anchor.workspace.AnchorStakingExample as Program<AnchorStakingExample>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+
+  it('is initialized!', async () => {
+    const stakingProgram = anchor.web3.Keypair.generate()
+    const poolOwner = (program.provider as anchor.AnchorProvider).wallet
+   
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        pool: stakingProgram.publicKey,
+        authority: poolOwner.publicKey,
+      })
+      .signers([stakingProgram])
+      .rpc()
+ 
+    console.log(`tx ${tx}`)
+ 
+    const state = await program.account.pool.fetch(stakingProgram.publicKey)
+    console.log(`state ${JSON.stringify(state)}`)
+
+    expect(state.authority.toString()).to.equal(poolOwner.publicKey.toString());
+    expect(state.userCount).to.equal(0);
+  })
 });
