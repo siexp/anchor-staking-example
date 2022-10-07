98a98880c6af12c987a4fe206747a034e085638c
Mark Tsyrulnyk <elioni17@gmail.com>
2022-10-07 16:50:15 +0300

    init function

--- programs/anchor-staking-example/src/lib.rs
+++ programs/anchor-staking-example/src/lib.rs
@@ -7,9 +7,25 @@ pub mod anchor_staking_example {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.pool.authority = ctx.accounts.authority.key();
+        ctx.accounts.pool.user_count = 0u32;
         Ok(())
-    }
+    }    
+}
+
+pub const POOL_STORAGE_TOTAL_BYTES: usize = 32 + 4;
+ 
+#[account]
+pub struct Pool {
+    pub authority: Pubkey,
+    pub user_count: u32,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = authority, space = 8 + POOL_STORAGE_TOTAL_BYTES)]
+    pub pool: Account<'info, Pool>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
