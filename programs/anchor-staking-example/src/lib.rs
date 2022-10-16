50d12c3483519ad81d0a1d64fd992828b1296bee
Mark Tsyrulnyk <elioni17@gmail.com>
2022-10-16 19:32:55 +0300

    create user pda

--- programs/anchor-staking-example/src/lib.rs
+++ programs/anchor-staking-example/src/lib.rs
@@ -10,7 +10,16 @@ pub mod anchor_staking_example {
         ctx.accounts.pool.authority = ctx.accounts.authority.key();
         ctx.accounts.pool.user_count = 0u32;
         Ok(())
-    }    
+    }
+
+    pub fn create_user(ctx: Context<CreateUser>) -> Result<()> {
+        let user = &mut ctx.accounts.user;
+        user.stake = 0u64;
+        user.bump = *ctx.bumps.get("user").unwrap();
+ 
+        ctx.accounts.pool.user_count = ctx.accounts.pool.user_count + 1;
+        Ok(())
+    }
 }
 
 pub const POOL_STORAGE_TOTAL_BYTES: usize = 32 + 4;
@@ -28,4 +37,29 @@ pub struct Initialize<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>
+}
+
+pub const USER_STORAGE_TOTAL_BYTES: usize = 1 + 8;
+ 
+#[account]
+pub struct User {
+    bump: u8,
+    stake: u64,
+}
+ 
+#[derive(Accounts)]
+pub struct CreateUser<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + USER_STORAGE_TOTAL_BYTES,
+        seeds = [b"user", authority.key().as_ref()],
+        bump
+    )]
+    pub user: Account<'info, User>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    #[account(mut)]
+    pub pool: Account<'info, Pool>,
+    pub system_program: Program<'info, System>,
 }
--- tests/anchor-staking-example.ts
+++ tests/anchor-staking-example.ts
@@ -3,17 +3,15 @@ import { Program } from "@project-serum/anchor";
 import { AnchorStakingExample } from "../target/types/anchor_staking_example";
 import { expect } from 'chai'
 
-describe("anchor-staking-example", () => {
+describe("staking", async () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.AnchorStakingExample as Program<AnchorStakingExample>;
-
-
+  const stakingProgram = anchor.web3.Keypair.generate()
+  const poolOwner = (program.provider as anchor.AnchorProvider).wallet
+ 
   it('is initialized!', async () => {
-    const stakingProgram = anchor.web3.Keypair.generate()
-    const poolOwner = (program.provider as anchor.AnchorProvider).wallet
-   
     const tx = await program.methods
       .initialize()
       .accounts({
@@ -30,5 +28,35 @@ describe("anchor-staking-example", () => {
 
     expect(state.authority.toString()).to.equal(poolOwner.publicKey.toString());
     expect(state.userCount).to.equal(0);
+  }),
+
+  it('is user created!', async () => {
+    const userAccount = provider.wallet
+    const [userPDA, _] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        anchor.utils.bytes.utf8.encode('user'),
+        userAccount.publicKey.toBuffer(),
+      ],
+      program.programId
+    )
+
+    const tx = await program.methods
+      .createUser()
+      .accounts({
+        user: userPDA,
+        authority: userAccount.publicKey,
+        pool: stakingProgram.publicKey,
+      })
+      .rpc()
+
+    console.log(`tx ${tx}`)
+
+    const state = await program.account.pool.fetch(stakingProgram.publicKey)
+    console.log(`state ${JSON.stringify(state)}`)
+    expect(state.userCount).to.equal(1)
+ 
+    const user = await program.account.user.fetch(userPDA)
+    console.log(`user ${JSON.stringify(user)}`)
+    expect(user.stake.toNumber()).to.equal(0)
   })
 });
